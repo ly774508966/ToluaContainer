68a26a8c99b66cc118dd64f0d0fe31534b90dd68
joey1258 <1260216329@qq.com>
2016-09-14 17:19:23 +0800

    2016.9.14.17:18
    fix

--- Assets/LuaContainer/Extensions/Editor/ContextRoots/ContextRootEditor.cs
+++ Assets/LuaContainer/Extensions/Editor/ContextRoots/ContextRootEditor.cs
@@ -1,17 +1,23 @@
 ﻿/*
- * Copyright 2016 Sun Ning（Joey1258）
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the License is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the License for the specific language governing permissions and
- *      limitations under the License.
+ * Copyright (c) 2016 Joey1258
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 
 using UnityEngine;
@@ -19,9 +25,9 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using System;
 using System.Collections.Generic;
-using uMVVMCS.DIContainer;
+using LuaContainer.Container;
 
-namespace uMVVMCS.Editors
+namespace LuaContainer.Editors
 {
     [CustomEditor(typeof(ContextRoot), true)]
     public class ContextRootEditor : Editor
@@ -59,8 +65,8 @@ namespace uMVVMCS.Editors
             int length = customTypes.Length;
             for (int i = 0; i < length; i++)
             {
-                // 如果不是 uMVVMCS 命名空间下的类型才添加到 customScriptsNames
-                if (!customTypes[i].FullName.StartsWith("uMVVMCS"))
+                // 如果不是 LuaContainer 命名空间下的类型才添加到 customScriptsNames
+                if (!customTypes[i].FullName.StartsWith("LuaContainer"))
                 {
                     customScriptsNames.Add(customTypes[i].FullName);
                 }
